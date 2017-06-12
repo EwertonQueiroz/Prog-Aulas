d572ae1fea3d4645f104c99e53fdc64d59a16f87
Ewerton Queiroz <ewerton@debian>
2017-06-12 20:07:51 -0300

    Aula Classes Abstratas 12/06

--- src/poo/Conta.java
+++ src/poo/Conta.java
@@ -1,54 +1,18 @@
 package poo;
 
-public class Conta {
-	private String numero;
-	private double saldo;
+public class Conta extends ContaAbstrata {
 	
 	public Conta () {}
 	
 	public Conta (String numero, double saldo) {
-		this.numero = numero;
-		this.saldo = saldo;
-	}
-	
-	public String getNumero() {
-		return this.numero;
-	}
-	
-	public double getSaldo() {
-		return this.saldo;
-	}
-	
-	public void setNumero (String numero) {
-		this.numero = numero;
-	}
-	
-	public void setSaldo (double saldo) {
-		if (saldo > 0) {
-			this.saldo = saldo;
-		} else {
-			System.out.println("N�o � poss�vel saldo negativo!");
-		}
+		super(numero, saldo);
 	}
 	
 	public void sacar (double quantidade) {
-		if (quantidade > this.saldo) {
+		if (quantidade > super.getSaldo()) {
 			System.out.println("N�o posso sacar fora do limite!");
 		} else {
-			this.saldo -= quantidade;
-		}
-	}
-	
-	public void creditar (double quantidade) {
-		this.saldo += quantidade;
-	}
-	
-	public void transferir (Conta conta_destino, double qtd) {
-		if (this.getSaldo() < qtd) {
-			System.out.println("Saldo insuficiente!");
-		} else {
-			conta_destino.creditar(qtd);
-			this.sacar(qtd);
+			super.setSaldo(super.getSaldo() - quantidade);
 		}
 	}
 	
